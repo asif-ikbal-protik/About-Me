50beeafff4e8372da19c6fd017c833e90a7be378
asif-ikbal-protik <sheikhprotik@gmail.com>
2025-07-13 02:12:56 +0600

    feat: simplify router and set base path

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route, Router, useLocation } from "wouter"; // 1. Import useLocation
+import { Switch, Route, Router } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -6,26 +6,21 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import Home from "@/pages/home";
 import NotFound from "@/pages/not-found";
 
-function AppRouter() {
-  const [location] = useLocation();
-
-  // Add the ': string' type annotation here
-  const nested = (path: string) => `${location}${path}`.replace(/\/+/g, "/");
-
-  return (
-    <Switch>
-      <Route path={nested("/")} component={Home} />
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
-
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
-        <AppRouter />
+        {/*
+          The Router component tells all child routes to work from this base path.
+          This is the standard fix for deploying to a sub-folder like /About-Me/.
+        */}
+        <Router base="/About-Me">
+          <Switch>
+            <Route path="/" component={Home} />
+            <Route component={NotFound} />
+          </Switch>
+        </Router>
       </TooltipProvider>
     </QueryClientProvider>
   );
