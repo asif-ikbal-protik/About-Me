ab61cccdcc9c7fea746e4ced684bc4fdb997f709
asif-ikbal-protik <sheikhprotik@gmail.com>
2025-07-13 01:30:08 +0600

    fix: set base path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
+  base: "/About-Me/", // <-- ADD THIS LINE
   plugins: [
     react(),
     runtimeErrorOverlay(),
