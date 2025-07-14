ba40b31fd6e90c1020e82ce4af59976678b0152f
asif-ikbal-protik <sheikhprotik@gmail.com>
2025-07-14 21:34:50 +0600

    feat: add github actions workflow for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,10 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: "/About-Me/",
   root: "client",
+  base: "/About-Me/",
   plugins: [react()],
   build: {
-    outDir: "../dist",
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
+    outDir: "./dist",
   },
 });
